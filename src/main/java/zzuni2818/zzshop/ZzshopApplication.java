19287347f503a254dd12c5188aabb757984956d5
zzuni2818 <zzuni2818@gmail.com>
2024-03-06 15:40:46 +0900

    init

---
+++ src/main/java/zzuni2818/zzshop/ZzshopApplication.java
@@ -0,0 +1,13 @@
+package zzuni2818.zzshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzshopApplication.class, args);
+	}
+
+}
